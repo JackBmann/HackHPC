d8d4c50c20925bad01090bce960f89e0003cead2
Ted Morin <tmorin@udallas.edu>
2018-11-14 13:31:39 -0600

    updated predictor02.py to predictor03.py - now offloads data to file

--- predictor03.py
+++ predictor03.py
@@ -7,14 +7,17 @@ fit stock data to review data
 (fit from multiple variables)
 (fit multiple variables, offload to file)
 
-Usage: python predictorXX.py stockFileName.csv reviewFileName.csv
+Usage: python predictorXX.py stockFileName.csv reviewFileName.csv outTag
 """
 
 
 import numpy as np
 import pandas as pd
 from sys import argv
-from sklearn import linear_model as lm
+#from sklearn import linear_model as lm
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 # input/output data
 stoDataName = argv[1]
@@ -51,14 +54,28 @@ for ix, rev in revData.iterrows():
         newData[revDataWidth-1][j] += 1
     for d in range(revDataWidth-1):
         newData[d][i] = newData[d][i]/newData[revDataWidth-1][i]
-#newData = np.array(newData)
+newData = np.array(newData).T
 
 # iterate over stocks and match
 fitData = []
 fitKeys = []
 for k in stoDataKeys:
     fitKeys.append(k)
-    fitData.append(lm.LinearRegression(newData, stoData[k].values))
+    # fit the data
+    #fitData.append(lm.LinearRegression(newData, stoData[k].values))
+    #print len(stoData[k].values), len(newData[0])
+    fitData.append(sm.OLS(stoData[k].values, newData).fit())
+    # test how the regression performs
+    prediction = fitData[-1].predict(newData)
+    with open(outTag+"_model_"+k+"_"+str(datetime.now())[:10]+".csv",'w') as outf:
+        outf.write(k+'\n')
+        outf.write(str(newKeys))
+        outf.write(fitData[-1].params)
+    with open(outTag+"_predict_"+k+"_"+str(datetime.now())[:10]+".csv",'w') as outf:
+        outf.write(k)
+        for pred in list(prediction):
+            outf.write(str(pred))
+    #plt.plot(stoData)
+    #plt.plot(stoData[stoDataKeys[0]], prediction)
+    #plt.show()
 
-print fitKeys
-print fitData
